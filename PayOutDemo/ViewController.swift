5eb3099686c5e7f0974eed8985e011df061fbcbd
Marek Hajdučák <marekhajducak@Mareks-MacBook-Pro.local>
2019-03-18 11:08:45 +0100

    Initial project

---
+++ PayOutDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PayOutDemo
+//
+//  Created by Marek Hajdučák on 18/03/2019.
+//  Copyright © 2019 Marek Hajdučák. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
